0487b968eb468462999c08a9ed006453db5078bb
Ariel <you@example.com>
2025-04-22 13:16:30 -0300

    iniciando os ajustes em redux

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,17 @@
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+// import { Produto } from '../../App'
+
+// type CarrinhoState = {
+//     itens: Produto[]
+// }
+
+// const initialState: CarrinhoState = {
+//     itens: []
+// }
+
+// const carrinhoSlice = createSlice({
+//     name: 'carrinho',
+//     initialState,
+//     reducers: {adicionar: (state, action: PayloadAction<Game>) => {
+//         const jogo = action.payload
